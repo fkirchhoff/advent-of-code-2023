eb1d430f1afc69bd8369461dd63ee7014487fc15
fkirchhoff <florian.kirchhoff@gmail.com>
2023-12-02 15:44:56 -0500

    Throw RuntimeException instead of checked.

--- src/main/java/common/InputReader.java
+++ src/main/java/common/InputReader.java
@@ -1,31 +1,34 @@
 package common;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.List;
 
 public class InputReader {
 
-
-    public static List<String> readAllLines(String input) throws IOException, ClassNotFoundException {
+    public static List<String> readAllLines(String input) {
         // Get the call stack
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        try {
 
-        // The calling class is at index 2 (index 0 and 1 are related to getStackTrace())
-        if (stackTrace.length >= 3) {
-            var callingClassName = stackTrace[2].getClassName();
-            var callingClass = Class.forName(callingClassName);
-            var stream = callingClass.getResourceAsStream(input);
-            if(stream != null) {
-                var reader = new InputStreamReader(stream);
-                // read the file line by line
-                List<String> lines = new java.io.BufferedReader(reader).lines().toList();
-                return lines;
+            // The calling class is at index 2 (index 0 and 1 are related to getStackTrace())
+            if (stackTrace.length >= 3) {
+                var callingClassName = stackTrace[2].getClassName();
+                Class<?> callingClass = null;
+                callingClass = Class.forName(callingClassName);
+                var stream = callingClass.getResourceAsStream(input);
+                if (stream != null) {
+                    var reader = new InputStreamReader(stream);
+                    // read the file line by line
+                    List<String> lines = new java.io.BufferedReader(reader).lines().toList();
+                    return lines;
+                }
             }
+            return Files.readAllLines(java.nio.file.Path.of(input));
+        } catch (Exception e) {
+            throw new RuntimeException("Could not read lines from " + input, e);
         }
-        return Files.readAllLines(java.nio.file.Path.of(input));
-    
+
     }
-    
+
 }
